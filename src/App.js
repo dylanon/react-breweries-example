1e726e27d99ec7111e10591384d00a83fdc382c8
Dylan On <dylanon@users.noreply.github.com>
2019-04-12 19:53:20 -0400

    Show list of breweries

--- src/App.js
+++ src/App.js
@@ -3,23 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    console.log('constructor was called')
+    super(props);
+    this.state = {
+      breweries: [],
+      isLoading: false,
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.setState({
+      isLoading: true
+    })
+    fetch('https://api.openbrewerydb.org/breweries')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          breweries: data,
+          isLoading: false
+        });
+      })
+  }
+
   render() {
+    console.log('render was called');
+    const { breweries, isLoading } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>BREWERIES 🍺</h1>
+        {isLoading ? (
+          <img src={logo} className="App-logo" alt="logo"/>
+        ) : (
+          <ul>
+            {breweries.map(brewery => {
+              const { id, name, website_url, city, state } = brewery
+              return (
+                <li key={id}>
+                  <a href={website_url} target="blank">{name}</a>
+                  <p>{city}, {state}</p>
+                </li>
+              )
+            })}
+          </ul>
+        )}
       </div>
     );
   }
